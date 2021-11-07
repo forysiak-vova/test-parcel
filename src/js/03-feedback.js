e650fdfc8ce1b6cbd7c3a7b359bdaad2eb426212
forysiak-vova <volodja.forus@gmail.com>
2021-11-07 14:51:12 +0100

    завдання 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,22 +1,48 @@
+import throttle from 'lodash.throttle'
 import '../css/common.css';
 import '../css/03-feedback.css'
 
 const refs = {
    form: document.querySelector('.feedback-form'),
-   textarea: document.querySelector('.feedback-form textarea')
+   textarea: document.querySelector('.feedback-form textarea'),
+   input: document.querySelector('.feedback-form input')
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', onTexareaInput);
+refs.textarea.addEventListener('input', throttle(onTexareaInput, 500));
+refs.input.addEventListener('input', onInputEmail);
 
-function onFormSubmit(e) {
-  
+populateTexterea();
 
+
+function onInputEmail(e) {
+   const email = e.currentTarget.value;
+    localStorage.setItem('feedback-form-state1', email);
+   
 };
 
-
+function onFormSubmit(e) {
+   e.preventDefault();
+  
+   e.currentTarget.reset();
+   localStorage.removeItem('feedback-form-state');
+   localStorage.removeItem('feedback-form-state1');
+   console.log('відправляємо форму');
+   
+};
 
 function onTexareaInput(e) {
     const message = e.currentTarget.value;
    localStorage.setItem('feedback-form-state', message);
+};
+
+function populateTexterea(e) {
+   const savedMessage = localStorage.getItem('feedback-form-state');
+   const saveEmail = localStorage.getItem('feedback-form-state1');
+   if (savedMessage || saveEmail) {
+      console.log(saveEmail);
+      console.log(savedMessage);
+      refs.textarea.value = savedMessage;
+       refs.input.value = saveEmail;
+   };
 };
