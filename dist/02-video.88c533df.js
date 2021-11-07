d21a1016c2e7fa71b4b9f65f4b6d7119ac7e092f
forysiak-vova <volodja.forus@gmail.com>
2021-11-07 12:36:58 +0100

    t

--- dist/02-video.88c533df.js
+++ dist/02-video.88c533df.js
@@ -149,7 +149,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55719" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64294" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
