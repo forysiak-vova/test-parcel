bc1424e6d1b7b1905fc7727e86615e2baf08a030
forysiak-vova <volodja.forus@gmail.com>
2021-11-16 00:45:40 +0100

    e

--- dist/02-video.88c533df.js
+++ dist/02-video.88c533df.js
@@ -117,11 +117,130 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"js/02-video.js":[function(require,module,exports) {
-console.log(localStorage);
-localStorage.setItem('my-data', 'qweqweqwe');
-console.log(localStorage.getItem('my-data'));
-},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
+
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
+}
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"css/common.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/02-video.js":[function(require,module,exports) {
+"use strict";
+
+var _common = _interopRequireDefault(require("../css/common.css"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// console.log(localStorage);
+// localStorage.setItem('my-data', 'qweqweqwe');
+// console.log(localStorage.getItem('my-data'));
+// const logMessage = () => {
+//    console.log('Лог при визове функції через 3 сек');
+// };
+// console.log('До визова функції setTimeout');
+// setTimeout((x,y) => {
+//    console.log('всередині колбек для setTimeout');
+//    console.log(x,y);
+// }, 2000, 5, 50)
+//       console.log('После визова функції setTimeout');
+//  ===================================== setTimeout and clearTimeout  ================================
+// const logger = time => {
+//    console.log(`log через ${time} ms, тому що не відмінили таймаут`);
+// };
+// const timerId = setTimeout(logger, 2000, 2000);
+// console.log(timerId);
+// const showldChennelTimer = Math.random() > 0.3;
+// console.log(showldChennelTimer);
+// if (showldChennelTimer) {
+//    clearTimeout(timerId);
+//  };
+// ==================================== setInterval ====================================================
+// const logger = time => {
+//    console.log(`визов функції через інтервал ${time} ms`);
+// };
+// const intervalId = setInterval(logger, 2000, 2000);
+// const resOftrueFalse = Math.random() > 0.3;
+// console.log(resOftrueFalse);
+// if (resOftrueFalse) {
+//    clearInterval(intervalId);
+// }
+var total = 0;
+var timeId = setInterval(function () {
+  if (total === 3) {
+    console.log('видали');
+    clearInterval(timeId);
+    return;
+  }
+
+  console.log('hello');
+  total += 1;
+}, 1000);
+},{"../css/common.css":"css/common.css"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -149,7 +268,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62742" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61286" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,49 @@
-console.log(localStorage);
-localStorage.setItem('my-data', 'qweqweqwe');
-console.log(localStorage.getItem('my-data'));
+// console.log(localStorage);
+// localStorage.setItem('my-data', 'qweqweqwe');
+// console.log(localStorage.getItem('my-data'));
+import common from '../css/common.css'
+// const logMessage = () => {
+//    console.log('Лог при визове функції через 3 сек');
+// };
+// console.log('До визова функції setTimeout');
+
+// setTimeout((x,y) => {
+//    console.log('всередині колбек для setTimeout');
+//    console.log(x,y);
+// }, 2000, 5, 50)
+//       console.log('После визова функції setTimeout');
+//  ===================================== setTimeout and clearTimeout  ================================
+// const logger = time => {
+//    console.log(`log через ${time} ms, тому що не відмінили таймаут`);
+// };
+// const timerId = setTimeout(logger, 2000, 2000);
+// console.log(timerId);
+// const showldChennelTimer = Math.random() > 0.3;
+// console.log(showldChennelTimer);
+
+// if (showldChennelTimer) {
+//    clearTimeout(timerId);
+//  };
+
+// ==================================== setInterval ====================================================
+// const logger = time => {
+//    console.log(`визов функції через інтервал ${time} ms`);
+// };
+// const intervalId = setInterval(logger, 2000, 2000);
+
+// const resOftrueFalse = Math.random() > 0.3;
+// console.log(resOftrueFalse);
+// if (resOftrueFalse) {
+//    clearInterval(intervalId);
+// }
+let total = 0;
+const timeId = setInterval(() => {
+   if (total === 3) {
+       console.log('видали');
+      clearInterval(timeId);
+      return;
+   }
+     console.log('hello');
+   total += 1;
+ 
+},1000);
